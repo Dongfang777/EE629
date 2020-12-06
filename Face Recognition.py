d8f3b772d6f39d9a558c456e58b0d2f0ea0b3203
Dawnfinite <70695206+Dongfang777@users.noreply.github.com>
2020-12-06 20:23:38 +0800

    Update Face Recognition.py

--- Face Recognition.py
+++ Face Recognition.py
@@ -119,7 +119,7 @@ cap.release()
 
                   
 
- import os
+import os
 import cv2
 import sys
 from PIL import Image
@@ -156,3 +156,26 @@ if __name__ == '__main__':
     recognizer.train(faces,np.array(ids))
     # save files
     recognizer.write('trainer/trainer.yml')                 
+
+                  
+                  
+                  
+import cv2
+import numpy as np
+import os
+# Load the training dataset file
+recogizer=cv2.face.LBPHFaceRecognizer_create()
+recogizer.read('trainer/trainer.yml')
+img=cv2.imread('3.pgm')
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+face_detector = cv2.CascadeClassifier(
+    'E:/soft/opencv/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
+faces = face_detector.detectMultiScale(gray)
+for x,y,w,h in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    # Face Recognition
+    id,confidence=recogizer.predict(gray[y:y+h,x:x+w])
+    print('Label id:',id,'confidence score：',confidence)
+cv2.imshow('result',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
